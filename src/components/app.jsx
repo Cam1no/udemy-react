dc8227c51a6bc84e9b1583299d88251c879c3ca8
kinjoyuji <kinjoyuji@gmail.com>
2017-09-12 01:53:58 +0900

    不要なファイル/コードを削除

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,36 +1,14 @@
 import React, { Component } from 'react';
-import Greeting from './greeting'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: 'Bob',
-    };
-  }
-
-  handleMouseOver() {
-    this.setState({ name: 'Bob' });
-  }
-
-  handleMouseOut() {
-    this.setState({ name: 'Jhoe' });
-  }
-
-  handleOnChange(name) {
-    this.setState({ name: name });
-  }
-
-  handleButtonClick() {
-    this.setState({ name: 'BoooooooooB' })
+    this.state = {};
   }
 
   render() {
     return (
       <div>
-        <input type="text" value={this.state.name} onChange={e => this.handleOnChange(e.target.value)} />
-        <button onClick={() => this.handleButtonClick()}/>
-        <Greeting name={this.state.name} />
       </div>
     );
   }
