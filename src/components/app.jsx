e6c4da02c6cc9019836c8d2fe4b35f260e18d0c9
kinjoyuji <kinjoyuji@gmail.com>
2017-09-12 00:15:38 +0900

    イベントの作成

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import Greeting from './greeting'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Bob',
+    };
+  }
+
+  handleMouseOver() {
+    this.setState({ name: 'Bob' });
+  }
+
+  handleMouseOut() {
+    this.setState({ name: 'Jhoe' });
+  }
+
   render() {
     return (
-      <div>
-        {this.props.hi}
+      <div
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+      >
+        <Greeting name={this.state.name} />
       </div>
     );
   }
