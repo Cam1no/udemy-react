8e4e00662ae65d95e453d9f8b7fb76ef98080e34
kinjoyuji <kinjoyuji@gmail.com>
2017-09-12 03:44:17 +0900

    geocode componentの実装

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +17,9 @@ class App extends Component {
       <div>
         <h1>緯度経度検索</h1>
         <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
+        <GeocodeResult address={this.state.address}
+                       lat={this.state.lat}
+                       lng={this.state.lng}/>
       </div>
     );
   }
