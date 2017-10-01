ff7736bbb69c34f9cd7ea293d636da1bc09dbf6f
kinjoyuji <kinjoyuji@gmail.com>
2017-10-01 20:40:10 +0900

    bind method

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,6 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handlePlaceSubmit = this.handlePlaceSubmit.bind(this);
   }
 
   handlePlaceSubmit(place) {
